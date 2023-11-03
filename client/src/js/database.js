d99d2b473bf31f950cf5b8f45b5a747715d352d7
Ravneet Kaur <ravneet.kaur9629@gmail.com>
2023-11-02 20:15:11 -0700

    corrected serviceworker and database name

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,14 +1,14 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('notepad', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('notepad')) {
+        console.log('notepad database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('notepad', { keyPath: 'id', autoIncrement: true });
+      console.log('notepad database created');
     },
   });
 
